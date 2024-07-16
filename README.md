6b8503fd641763b7715441794cbe170e3baed799
PARAS SAXENA <parassaxenaofficial@gmail.com>
2024-07-16 15:57:56 +0530

    Finalized Rev-Assist.
    Finalized & Some Minor Improvements

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Rev-Assistant
-This educational website focuses on providing a unique learning experience for students. Smart Assist, the core feature, leverages natural language processing and deep learning techniques to generate concise summaries of lengthy textual paragraphs and video lectures.
+
+# Smart-Assist
+
+This is the github repositry of a website made by Team Hufflepuffs(group of students of IIT Mandi) for the event frosthack in the technical fest of IIT Mandi XPECTO'22.
+This website is based on the theme education and is particulary for student's use.
+Smart assist basically creates a study environment for the students and ease their problem of reading long textual paragraphs or listening to long video lectures by provide a short summary of the whole.
+This summary is processed by using natural language processing and deep learning. By the use of natural language toolkit we can find the important questions with their answers and some important keywords related to the paragraph.
+
+# Objective
+1. It is helpful for endtime revision for a student. Now students can get short summary, important questions and answers, keywords and their meanings from
+   video lectures, audio lectures and texts.
+2. Sometimes it’s too boring to read long long paragraphs, watching long videos, or hearing long audios. And sometimes even we do not have enough time for      the same.
+3. It is also helpful for the teachers to generate questions for the exams by using their own video lectures itself.
+
+# Techstack
+1. NLP - Natural Language Processing for various features
+2. Flask - Backend
+3. Tensorflow and Pytorch - Libraries to implement NLP
+4. HTML, CSS, JavaScript - Frontend
+
+# Working
+1. First you have to clone the repository.
+2. If you don't have a virtual environment then run the command `pip install virtualenv`.
+3. Create virtual environment by using `python -m virtalenv env` and then `source env/Scripts/activate`(for Windows Users) and `source ./env/bin/activate`(for Linux users).
+4. Next use the `pip install -r requirements.txt` command or `pip3 install requirements.txt` to install all the libraries and dependencies
+5. Now write the commands `pip install transformers==3.0.0` ,`pip install nltk` and `pip install nlp`.
+6. After installing nltk do `python -m nltk.downloader punkt`
+7. Now to run the website go inside the smart assist directory and type `python app.py`.
+8. Now the website will start running locally and bingo u can enjoy the various features of the website.
+
+# Video Demo
+https://youtu.be/t4hJB6XJ4sY 
+
+Watch the video at 1.25 or 1.5x for better experience :-)
+
+# Landing Page :
+![Screenshot 2022-05-23 at 2 00 29 AM](https://user-images.githubusercontent.com/79039452/169714623-b1be991f-6bbd-4216-b599-88567106d37d.png)
+# Video/Text/Audio uploading page:
+![Screenshot 2022-05-23 at 2 01 02 AM](https://user-images.githubusercontent.com/79039452/169714646-c5fd62c2-47ab-4261-bd9a-07a34be6ee3d.png)
+# Summarization Result:
+![Screenshot 2022-05-23 at 3 36 49 AM](https://user-images.githubusercontent.com/79039452/169718042-1b5a0632-a7e7-43c1-97d2-37ddd9a62766.png)
+# Question Answer Generation:
+![Screenshot 2022-05-23 at 2 20 05 AM](https://user-images.githubusercontent.com/79039452/169715275-8cffede3-4d8f-4f0d-bb99-ed1e3897f1fe.png)
+# Important Tools:
+![Screenshot 2022-05-23 at 2 02 09 AM](https://user-images.githubusercontent.com/79039452/169714676-15c67781-a97e-4a7e-866a-92748615fea9.png)
+# About
+![Screenshot 2022-05-23 at 2 02 58 AM](https://user-images.githubusercontent.com/79039452/169714704-547ddf49-673e-46fc-b519-3814ff15a4f9.png)
---
+++ app.py
@@ -0,0 +1,116 @@
+from crypt import methods
+from operator import truediv
+from flask import *
+from transformers import pipeline
+import moviepy.editor
+from question_answer.question_generator import generate_questions
+from summary import summarizer
+import speech_recognition as sr
+import os
+import nltk
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+
+app = Flask(__name__)
+
+#video_to_audio
+
+def video_to_audio(path):
+    video = moviepy.editor.VideoFileClip(path)
+    aud = video.audio
+    aud.write_audiofile("demo.wav")
+    print("--End--")
+
+#audio_to_text
+def audio_to_text(path):
+    text=''
+    r=sr.Recognizer()
+    with sr.AudioFile(path) as source:
+        audio_data = r.record(source)
+        text = r.recognize_google(audio_data)
+        print(text)
+        return text
+
+# Keywords Finder
+def keyword_generator(text,range, top_n):
+    n_gram_range = (1, range)
+    stop_words = "english"
+    count = CountVectorizer(ngram_range=n_gram_range, stop_words=stop_words).fit([text])
+    candidates = count.get_feature_names_out()
+    model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+    doc_embedding = model.encode([text])
+    candidate_embeddings = model.encode(candidates)
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
+    keywords = [candidates[index] for index in distances.argsort()[0][-top_n:]]
+    print(keywords)
+    return keywords
+
+@app.route("/")
+def landing():
+    return render_template('home.html')
+
+@app.route("/home", methods = ['GET'])
+def home():
+    return render_template('upload.html')
+
+@app.route('/home', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        fileType = request.form.get('type')
+        if(fileType=="audio" or fileType=="video"):
+            f = request.files['file']
+            if(fileType=="video"):
+                f.save(f.filename)
+                path = f.filename
+                video = moviepy.editor.VideoFileClip(path)
+                video_to_audio(path)
+            else:
+                f.save("demo.wav")
+            text = audio_to_text("demo.wav")
+        # # summary_text = text_summary(text)
+        else:
+            text = request.form.get("inputText")
+        # Generating Questions
+        questions_data = generate_questions(text)
+        print(questions_data)
+        for item in questions_data:
+            item['question']=item['question'].replace("<pad>", "")
+            item['answer']=item['answer'].replace("<pad>", "")
+        # Generating Keywords
+        keywords=[]
+        keywords.append(keyword_generator(text, 1, 5))
+        keywords.append(keyword_generator(text, 2, 3))
+        keywords.append(keyword_generator(text, 3, 1))
+        ch1 = len(text);
+        w1 = len(text.split())
+        sentences = nltk.sent_tokenize(text)
+        summaryLen = len(sentences)
+        summary_text=summarizer(text, summaryLen//2)
+        ch2 = len(summary_text);
+        w2 = len(summary_text.split())
+        print(summary_text)
+
+        return render_template('summary.html', actualText = text, summary_text = summary_text, questions_data = questions_data, keywords = keywords, w1 = w1, w2 = w2, ch1 = ch1, ch2 = ch2)
+
+@app.route("/summary", methods = ['GET'])
+def summary():
+    return render_template('summary.html')
+
+@app.route("/graph", methods=['GET'])
+def graph():
+    return render_template('graph.html')
+
+
+@app.route("/tool", methods=['GET'])
+def tool():
+    return render_template('tools.html')
+
+@app.route("/calculator", methods=['GET'])
+def calculator():
+    return render_template('calculator.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
